3760bb0ed3eb1911a8639acfd11eabbd3849fcc1
zhen <zhen.yang@hotmail.fr>
2024-01-27 15:02:10 +0100

    maj

--- src/app/components/phonescreen/phonescreen.component.ts
+++ src/app/components/phonescreen/phonescreen.component.ts
@@ -4,6 +4,8 @@ import { IPhoneUser, IUser } from 'src/app/interfaces/general';
 import { SocketService } from 'src/app/services/socket.service';
 import { StoreService } from 'src/app/services/store.service';
 import { v4 as uuidv4 } from 'uuid';
+import Peer from 'peerjs';
+import { wait } from 'src/app/utils/generalUtil';
 
 @Component({
   selector: 'app-phonescreen',
@@ -20,6 +22,8 @@ export class PhonescreenComponent  implements OnInit, AfterViewInit, OnDestroy {
   @ViewChild('remoteVideo') remoteVideo: ElementRef<HTMLVideoElement>;
   @ViewChild('audiosong') audioSong: ElementRef<HTMLAudioElement>;
   communicating:boolean = false;
+  peer: Peer;
+
   constructor(
     private readonly storeService: StoreService,
     private readonly socketService: SocketService,
@@ -40,8 +44,11 @@ export class PhonescreenComponent  implements OnInit, AfterViewInit, OnDestroy {
       this.modal.dismiss();
     });
 
-    this.socketService.socket.on("server:call:sendPeerId", async(data:any) => {
+    this.socketService.socket.on("server:call:sendPeerId", async (data:any) => {
       this.hisPeerId = data['peerId'];
+      this.audioSong.nativeElement.pause();
+      await this.createPeer();
+      await this.callOtherByPeer();
     });
 
     this.storeService.isOnCalling$.next([true]);
@@ -73,12 +80,60 @@ export class PhonescreenComponent  implements OnInit, AfterViewInit, OnDestroy {
     this.modal.dismiss();
   }
 
-  acceptCall(): void {
+  async acceptCall(): Promise<void> {
     this.communicating = true;
     this.socketService.socket.emit("client:user:acceptCall", {
         caller: this.caller,
         called: this.called,
         peerId: this.myPeerId
     });
+    this.audioSong.nativeElement.pause();
+    await this.createPeer();
+    await this.callOtherByPeer();
+  }
+
+  async createPeer(): Promise<void> {
+    this.peer = new Peer(this.myPeerId);
+
+    this.peer.on("open", (id) => {
+      console.log(`My peer id : ${id}`);
+    });
+
+    //texte transféré
+    this.peer.on('connection', (connection) => {
+      connection.on('data', (data) => {});
+    });
+
+    //recieve
+    this.peer.on('call', async (call) => {
+      let stream = await navigator.mediaDevices.getUserMedia({ video: false, audio: true });
+      call.answer(stream);
+
+      call.on('stream', async (stream) => {
+        this.remoteVideo.nativeElement.srcObject = stream;
+        this.remoteVideo.nativeElement.pause();
+        await wait(0.15);
+        await this.remoteVideo.nativeElement.play();
+      });
+    });
+  }
+
+  async callOtherByPeer(): Promise<void> {
+    let stream = await navigator.mediaDevices.getUserMedia({ video: false, audio: true })
+    await wait(0.15);
+    //call another
+    const call = this.peer.call(this.hisPeerId, stream)
+    call.on("stream", async (stream) => {
+      this.remoteVideo.nativeElement.srcObject = stream;
+      this.remoteVideo.nativeElement.pause();
+      await wait(0.15);
+      await this.remoteVideo.nativeElement.play();
+    });
+    call.on("error", (err) => {
+      console.error(err);
+    });
+    call.on('close', () => {
+      //endCall()
+    })
   }
 }
