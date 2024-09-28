ab583b1db1acfb6ad2c2e5ae8185772dec86b700
zhen <zhen.yang@hotmail.fr>
2024-09-28 13:59:03 +0200

    change backend server

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,7 @@
 export const environment = {
   production: true,
-  baseUrl: "https://citation-nodejs-api.onrender.com",
+  // baseUrl: "https://citation-nodejs-api.onrender.com",
+  baseUrl: "https://citation.zhenyangnodejs.fr",
   aws_s3_accessKeyId:"",
   aws_s3_secretAccessKey:"",
   aws_s3_bucket:""
